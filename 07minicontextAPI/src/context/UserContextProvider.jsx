645b21b6cc58e4247d4ec4aada4c2346045d901d
AkankshaStack <akankshasingh1342@gmail.com>
2024-02-23 16:03:10 +0530

    Initial commit

---
+++ 07minicontextAPI/src/context/UserContextProvider.jsx
@@ -0,0 +1,11 @@
+import UserContext from "./UserContext";
+
+const UserContextProvider = ({ children }) => {
+    const [user, setUser] = useState(null)
+    return (
+        <UserContext.provider value={{ user, setUser }}>
+            {children}
+        </UserContext.provider>
+    )
+}
+export default UserContextProvider
