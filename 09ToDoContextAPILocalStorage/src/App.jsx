26b5f8e319bb8f19ecc8493d95d65507076ddce3
Akanksha Singh <akankshasingh1324@gmail.com>
2024-02-27 21:06:46 +0530

    todo context

---
+++ 09ToDoContextAPILocalStorage/src/App.jsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+     <h1 className='text-3xl font-bold  textw-500 bg-green-500 p-6'>Manage Your Todos</h1>
+    </>
+  )
+}
+
+export default App
