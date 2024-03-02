8977229e1fb2eda0cfd524cbd0cf6461d0ff284c
Akanksha Singh <akankshasingh1324@gmail.com>
2024-03-02 12:55:27 +0530

    Todo using redux

--- 10reduxToolkitTodo/src/App.jsx
+++ 10reduxToolkitTodo/src/App.jsx
@@ -1,11 +1,17 @@
-import "./App.css";
+
+import './App.css'
+import AddTodo from './components/AddTodo'
+import Todos from './components/Todo'
 
 function App() {
+  
   return (
     <>
-      <h1>Learn about Redux Toolkit</h1>
+      <h1>Learn about redux toolkit</h1>
+      <AddTodo />
+      <Todos />
     </>
-  );
+  )
 }
 
-export default App;
+export default App
